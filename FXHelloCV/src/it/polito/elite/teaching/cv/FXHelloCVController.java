21ffd2b2c521922d556a19144645100440e07031
diegobug <diegoifrn@gmail.com>
2018-04-23 20:58:35 -0300

    1.0

--- FXHelloCV/src/it/polito/elite/teaching/cv/FXHelloCVController.java
+++ FXHelloCV/src/it/polito/elite/teaching/cv/FXHelloCVController.java
@@ -1,10 +1,19 @@
 package it.polito.elite.teaching.cv;
 
+import java.util.ArrayList;
+//import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.videoio.VideoCapture;
 
@@ -20,10 +29,9 @@ import javafx.scene.image.ImageView;
  * implemented. It handles the button for starting/stopping the camera and the
  * acquired video stream.
  *
- * @author <a href="mailto:luigi.derussis@polito.it">Luigi De Russis</a>
- * @author <a href="http://max-z.de">Maximilian Zuleger</a> (minor fixes)
- * @version 2.0 (2016-09-17)
- * @since 1.0 (2013-10-20)
+ * @author <a href="mailto:diegoifrn@gmail.com">Diego Lemos</a>
+ * @version 1.0 (2018-04-23)
+ * @since 1.0 (2018-04-23)
  *
  */
 public class FXHelloCVController
@@ -71,6 +79,7 @@ public class FXHelloCVController
 					{
 						// effectively grab and process a single frame
 						Mat frame = grabFrame();
+						//
 						// convert and show the frame
 						Image imageToShow = Utils.mat2Image(frame);
 						updateImageView(currentFrame, imageToShow);
@@ -183,5 +192,90 @@ public class FXHelloCVController
 	{
 		this.stopAcquisition();
 	}
-	
+	/**
+	 * It analyzes if the contour is a square 
+	 */
+	public static boolean isContourSquare(MatOfPoint thisContour) {
+
+	    Rect ret = null;
+
+	    MatOfPoint2f thisContour2f = new MatOfPoint2f();
+	    MatOfPoint approxContour = new MatOfPoint();
+	    MatOfPoint2f approxContour2f = new MatOfPoint2f();
+
+	    thisContour.convertTo(thisContour2f, CvType.CV_32FC2);
+
+	    Imgproc.approxPolyDP(thisContour2f, approxContour2f, 2, true);
+
+	    approxContour2f.convertTo(approxContour, CvType.CV_32S);
+
+	    if (approxContour.size().height == 4) {
+	        ret = Imgproc.boundingRect(approxContour);
+	    }
+
+	    return (ret != null);
+	}
+	/**
+	 * 
+	 */
+	public static MatOfPoint getSquareContours(List<MatOfPoint> contours) {
+
+	    MatOfPoint squares = null;
+	    for (MatOfPoint c : contours) {
+	        if (isContourSquare(c)) 
+	            if (squares == null)
+	                squares = c;      
+	    }
+
+	    return squares;
+	}
+	/**
+	 * processar a imagem
+	 */
+	public void process(Mat rgbaImage) 
+    {
+		Mat mPyrDownMat = rgbaImage;
+        //Imgproc.pyrDown(rgbaImage, mPyrDownMat);
+       // Imgproc.pyrDown(mPyrDownMat, mPyrDownMat);
+		Mat mHsvMat = new Mat();//se der errado, tirar new
+		Mat mMask = new Mat();//se der errado, tirar new
+		Mat mDilatedMask = new Mat();//se der errado, tirar new
+		Mat mHierarchy = new Mat();//se der errado, tirar new
+    Imgproc.cvtColor(mPyrDownMat, mHsvMat, Imgproc.COLOR_RGB2HSV_FULL);
+
+    Core.inRange(mHsvMat, new Scalar(0,0,200), new Scalar(0,0,255), mMask);//achar cor vermelha
+    Imgproc.dilate(mMask, mDilatedMask, new Mat());
+
+    List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+
+    Imgproc.findContours(mDilatedMask, contours, mHierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+    MatOfPoint squareContours = getSquareContours(contours);//junção aqui
+    
+    // Find max contour area
+  //  double maxArea = 0;
+    //Iterator<MatOfPoint> each = contours.iterator();
+   // while (each.hasNext()) 
+   // {
+       // MatOfPoint wrapper = each.next();
+   //     double area = Imgproc.contourArea(squareContours);
+   //     if (area > maxArea)
+   //         maxArea = area;
+   // }
+
+    //Imgproc.approxPolyDP(mSpectrum, approxCurve, epsilon, closed);
+
+    // Filter contours by area and resize to fit the original image size
+    contours.clear();
+    //each = contours.iterator();
+
+   // while (each.hasNext()) 
+   // {
+     //   MatOfPoint contour = each.next();
+       // if (Imgproc.contourArea(contour) > mMinContourArea*maxArea) 
+       // {
+            Core.multiply(squareContours, new Scalar(4,4), squareContours);
+            contours.add(squareContours);
+       // }
+   // }
+} 
 }
